9b4253930790764f85b12fcda8b2370692faa430
Claude Feldges <claude.feldges@gmail.com>
2024-12-13 15:52:09 +0100

    Minor refactoring.

--- frontend/fasthtml/storm_fasthtml.py
+++ frontend/fasthtml/storm_fasthtml.py
@@ -47,8 +47,9 @@ hdrs = (MarkdownJS(), Style(scroll_behaviour))#, scripts, link_daisyui, link_pic
 app, rt = fast_app(pico=True, hdrs=hdrs)
 
 # Read data from the source directory and return a dictionary under table
+# Deprecated - will be removed but the local_dir is still used in the code (while inactive),
+# So it was decided to keep it for now.
 local_dir = os.path.join(demo_util.get_demo_dir(), "DEMO_WORKING_DIR")
-os.makedirs(local_dir, exist_ok=True)
 
 #-------------------------------------------------------------------------------
 # Initiate the runner
@@ -501,9 +502,6 @@ def post(opportunity_id: str):
 def run_workflow(opportunity_name, opportunity_id):
     # Initiate the workflow
     if generation_status[opportunity_id] == 'initiated':
-    #    current_working_dir = os.path.join(demo_util.get_demo_dir(), "DEMO_WORKING_DIR")
-    #    if not os.path.exists(current_working_dir):
-    #        os.makedirs(current_working_dir)
         demo_util.set_storm_runner()
         generation_status[opportunity_id] = 'pre_writing'
     # Pre-writing
