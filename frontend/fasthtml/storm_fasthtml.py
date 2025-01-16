453b8d52f70435a2f50ea00e24c583b18b9af3a4
Claude Feldges <claude.feldges@gmail.com>
2025-01-16 15:01:30 +0100

    Changed the default port.

--- frontend/fasthtml/storm_fasthtml.py
+++ frontend/fasthtml/storm_fasthtml.py
@@ -1229,6 +1229,6 @@ def post():
 
 if __name__ == '__main__':
     host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8080))
     reload = os.getenv('RELOAD', 'true').lower() == 'true'
     serve(host=host, port=port, reload=reload)
